f93c8932d359ea24f2fde2f996f8765c9561c38a
twtrubiks <twtrubiks@gmail.com>
2026-05-09 07:25:09 +0000

    feat(timer): 加入 timerStore 與 TimerDial 元件

---
+++ src/lib/stores/timerStore.svelte.ts
@@ -0,0 +1,59 @@
+import { audioEngine } from '../audio/AudioEngine';
+import { audioStore } from './audioStore.svelte';
+import { storyStore } from './storyStore.svelte';
+import { SleepTimer } from '../timer/SleepTimer';
+import { settingsRepo } from '../storage/SettingsRepo';
+
+class TimerStore {
+  remainingSec = $state(0);
+  running = $state(false);
+  fadeOutSec = $state(30);
+  private timer: SleepTimer;
+  private tickHandle: ReturnType<typeof setInterval> | null = null;
+
+  constructor() {
+    this.timer = new SleepTimer({
+      fadeOut: (sec) => audioEngine.masterFadeOut(sec),
+      stopAll: () => {
+        storyStore.stop();
+        void audioStore.stopAll(0);
+      }
+    });
+    void this.loadSettings();
+  }
+
+  private async loadSettings() {
+    const s = await settingsRepo.load();
+    this.fadeOutSec = s.fadeOutOnTimerSec;
+  }
+
+  start(totalMin: number) {
+    this.timer.start({ totalSec: totalMin * 60, fadeOutSec: this.fadeOutSec });
+    this.running = true;
+    this.startTick();
+  }
+
+  cancel() {
+    this.timer.cancel();
+    this.running = false;
+    this.stopTick();
+    this.remainingSec = 0;
+  }
+
+  private startTick() {
+    this.stopTick();
+    this.tickHandle = setInterval(() => {
+      this.remainingSec = this.timer.remaining();
+      if (this.remainingSec === 0) {
+        this.running = false;
+        this.stopTick();
+      }
+    }, 250);
+  }
+
+  private stopTick() {
+    if (this.tickHandle) { clearInterval(this.tickHandle); this.tickHandle = null; }
+  }
+}
+
+export const timerStore = new TimerStore();
