5b786a14011545ad560b520ab1bdb7279e39e5c8
twtrubiks <twtrubiks@gmail.com>
2026-06-20 08:14:38 +0800

    enable github action

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "/",
+  base: "/moonseal/",
   plugins: [
     svelte(),
     VitePWA({
